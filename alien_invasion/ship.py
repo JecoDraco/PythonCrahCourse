261e205dd9911c5d0027fb2c72d4912808eb0800
JecoDraco <jeco2002eduardo@gmail.com>
2026-03-21 10:48:06 -0600

    Implementacion de limites de la pantalla, ademas de agregar configuracion de velovidad

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
@@ -15,19 +16,24 @@ class Ship:
         #Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        #Movement flag
+        #Store a decimal value for the ship's horizontal postion.
+        self.x = float(self.rect.x)
+
+        #Señales de moviemiento
         self.moving_right = False
         self.moving_left = False
 
     
     def update(self):
-        """Actualiza la posicion de la nave baso en la abndera de moviento
-        o señal?"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
-    
+        """Actualiza la posicion de la nave en base a la señal"""
+        #Actualiza el valor de X de la nave, no del rectangulo
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #Actualiza el rectangulo(conetendor) con el valor de x de la nave
+        self.rect.x = self.x     
 
     def blitme(self):
         """Draw the ship at its current location"""

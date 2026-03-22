e897653e16826fd9154902ee0866b5398003913d
JecoDraco <jeco2002eduardo@gmail.com>
2026-03-21 19:56:36 -0600

    Se creo la clase bala

---
+++ alien_invasion/bullet.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """Es la clase que maneja las balas que dispara la nave"""
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        #Crea un rectangulo de bala en la posicion (0,0) 
+        #y luego lo mueve a la posicion correcta
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        #Guarda la posicion de la bala con un valor decimal
+        self.y = float(self.rect.y)
+
+    
+    def update(self):
+        """Mueve la balla por la pantalla"""
+        #Actualiza el valor decimal de la bala
+        self.y -= self.settings.bullet_speed
+        #Actualiza la pocision del rectangulo
+        self.rect.y = self.y
+
+    
+    def draw_bullet(self):
+        """Dibuja la bala en la pantalla"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -9,5 +9,11 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230,230,230)
 
-        #Configuraciones de la nave
+        #Parametros de la nave
         self.ship_speed = 1.5
+
+        #Parametros de la bala
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
